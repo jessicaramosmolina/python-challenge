ae7b7178718bcb47a58bd6b3e14fae8a86ecb1cc
Jessica Ramos Molina <jessicaramosmolina@Jessicas-Air.hsd1.ca.comcast.net>
2020-11-05 20:39:52 -0800

    message

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,1 +1,65 @@
 #main script to run analysis
+#total months in csv file
+#sum of of total net profit and loss
+#calculate the average change
+#pull max value in csv file
+#pull min value in csv file
+
+import csv
+import os
+import sys
+
+
+csv_path = os.path.join('Resources/budget_data.csv')
+
+month_count = 0
+total_profit = 0
+last_profit = 0
+max_value = -9999999
+max_month = "" 
+min_value = 9999999
+min_month = ""
+total_change = 0
+average = 0
+profit = 0
+profit_list=[]
+profit_change_list=[]
+total_profit_change=0
+month_list=[]
+
+with open(csv_path,'r') as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=',')
+    csv_reader = next(csvreader) 
+
+
+    for row in csvreader:
+        month_count += 1
+        if month_count == 1:
+            last_profit = int(row[1])
+            total_profit += int(row[1])
+        else:
+            if (int(row[1]) - last_profit) > max_value:
+                max_value = int(row[1]) - last_profit
+                max_month = row[0]
+            if (int(row[1]) - last_profit) < min_value:
+                min_value = int(row[1]) - last_profit
+                min_month = row[0]
+            total_change += int(row[1]) - last_profit
+            total_profit += int(row[1])
+            last_profit = int(row[1])
+
+
+    average = total_change/(month_count-1)
+    average = round(average,2)
+
+print('Financial Analysis')
+print("--------------------")
+print(f'Total Months: {month_count}')
+print(f'Total: ${total_profit}')
+print(f'Average Change: ${average}')
+print(f'Greatest Increase in Profits: {max_month}  (${max_value})')
+print(f'Greatest Decrease in Profits: {min_month} (${min_value})')
+
+stdoutOrigin=sys.stdout 
+txtPath = os.path.join("Analysis/financial_analysis.txt")
+sys.stdout = open(txtPath, "w")
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,1 +1,67 @@
 #main script to run for each analysis
+#The total number of votes cast
+#a complete list of candidates who recieved votes
+#the percentage of votes each candidates
+#the total number of votes each candidate won
+#the winner of the election based on popular vote
+
+import csv
+import os 
+import sys
+
+vote_count = 0
+k_candidate = 0
+c_candidate = 0
+l_candidate = 0
+o_candidate = 0
+
+candidates_list = []
+
+csvpath = os.path.join('Resources/election_data.csv')
+
+with open(csvpath, 'r') as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=",")
+    header = next(csvreader)
+
+    for row in csvreader:
+        vote_count += 1
+        if row[2] in candidates_list:
+            candidates_list.append(row[2])
+        if row[2] == "Khan":
+            k_candidate += 1
+        if row[2] == "Correy":
+            c_candidate += 1
+        if row[2] == "Li":
+            l_candidate += 1
+        if row[2] == "O'Tooley":
+            o_candidate += 1
+    
+
+    
+    print("Election Results")
+    print('------------------------')
+    print(f'Total Votes: {vote_count}')
+    print('------------------------')
+    print(f'Khan: {round((k_candidate/vote_count)*100)}% ({k_candidate})')
+    print(f'Correy: {round((c_candidate/vote_count)*100)}% ({c_candidate})')
+    print(f'Li: {round((l_candidate/vote_count)*100)}% ({l_candidate})')
+    print(f"O'Tooley: {round((o_candidate/vote_count)*100)}% ({o_candidate})")
+    print('------------------------')
+
+    if k_candidate > c_candidate and k_candidate > l_candidate and k_candidate > o_candidate:
+        print('Winner: Kahn')
+    elif c_candidate > k_candidate and c_candidate > l_candidate and c_candidate > o_candidate:
+        print('Winner: Correy')
+    elif l_candidate > k_candidate and l_candidate > c_candidate and l_candidate > o_candidate:
+        print('Winner: Li')
+    elif o_candidate > k_candidate and o_candidate > c_candidate and o_candidate > l_candidate:
+        print("Winner: O'Tooley")
+    print('------------------------')
+
+    stdoutOrigin=sys.stdout 
+txtPath = os.path.join("Analysis/election_analysis.txt")
+sys.stdout = open(txtPath, "w")
+
+    
+    
+    
